f41603d6da41fe9b9eb2296def0f224f5a2eff9b
imkei96 <72588798+imkei96@users.noreply.github.com>
2020-10-15 11:49:05 +0700

    Create hp.cpp

---
+++ hp.cpp
@@ -0,0 +1,56 @@
+#include <vector>
+#include <iostream>
+#include <string>
+//#include <numeric>
+
+using namespace std;
+
+// Main Program
+int main()
+{
+    // Deklarasi Variable
+    int length, input, count;
+    int min, temp;
+    vector<int> hp;
+	vector<int> input_int;
+
+    // Read Count
+    cin >> count;
+    
+    // Loop
+    for (int x = 0 ; x < count ; x++)
+    {
+        // Clear
+        input_int.clear();
+        
+        // Read Banyak Angka
+        cin >> length;
+
+        // Read Line of int
+        for(int i=0;i<length; i++)
+        {
+            cin >> input;
+            input_int.push_back(input);
+        }
+        
+        // Find Minimum HP
+        min = input_int[0];
+        temp = 0;
+        for (int i = 0; i < length; i++)
+        {
+            temp += input_int[i];
+            if (temp < min)
+                min = temp;
+        }
+        
+        // Masukan nilai min sebagai HP = 1 - min
+        hp.push_back(1 - min);
+    }
+    
+    //Print Result HP
+    for (int x = 0 ; x < count ; x++)
+    {
+        cout << "Case #" << x+1 << ": " <<  hp[x] << endl;
+    }
+    
+}
