0a1991905cb2a890b091e62b943339fa683d0bce
imkei96 <72588798+imkei96@users.noreply.github.com>
2020-10-23 19:56:49 +0700

    Update ball_pick_v3.cpp
    optimisation and ready for random unsorted input

--- ball_pick_v3.cpp
+++ ball_pick_v3.cpp
@@ -15,41 +15,54 @@ int main()
     //Loop
     for(int x=0;x<t;x++)
     {
+        // Input Length Array
         cin >> length;
-        
-        vector<int> array;
         vector<int> array_prime;
+        vector<int> count;
         
         int length_prime=0;
         long long int sum =0;
-        int count =0 ;
-        //Scan array
+        
+        // Scan setiap input
         for(int i=0;i<length;i++)
         {
             int input;
+            int found = 0;
             cin >> input;
-            array.push_back(input);
             
+            // Jika input pertama, tambahkan ke array_prime
             if(array_prime.empty() && i==0)
             {
                 array_prime.push_back(input);
+                count.push_back(1);
                 length_prime++;
             }
-            else if(array_prime[length_prime-1] != input)
+            // Cari apakah angka masukan ada di array_prime
+            else for(int j=0;j<length_prime;j++)
             {
-                array_prime.push_back(input);
-                length_prime++;
-                count=0;
-            }
-            else if(array_prime[length_prime-1] == input)
-            {
-                count++;   
+                // Jika found, tambah count
+                if(input == array_prime[j])
+                {
+                    count[j] += 1;
+                    found = 1;
+                }
             }
             
-            if(count>=2)
-                sum += count*(count-1)/2;
+            // Jika tidak found, tambahkan ke array_prime
+            if (found != 1 && i!=0)
+            {
+                array_prime.push_back(input);
+                count.push_back(1);
+                length_prime++;
+            }
         }
-
+        
+        // Ubah seluruh nilai count jadi sum
+        for (int i=0;i<length_prime;i++)
+        {
+            if(count[i]>=3)
+                sum += count[i]*(count[i]-1)*(count[i]-2)/6;
+        }
         cout <<"Case #"<<x+1<<": "<<sum<<"\n";
     }
 
